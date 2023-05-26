b04f24d99a95b40ae199a0421e20018198a62bbb
Gianou <david.gianadda@students.hevs.ch>
2023-05-26 19:12:39 +0200

    map as a component

--- Frontend/vue-leaflet/src/App.vue
+++ Frontend/vue-leaflet/src/App.vue
@@ -1,43 +1,14 @@
 <template>
-  <div id="container">
-    <div id="mapContainer"></div>
-  </div>
+  <my-map />
 </template>
-
 <script>
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import Map from './components/Map.vue';
 
 export default {
-  name: "Map",
-  data() {
-    return {
-      center: [46.30942535215705, 7.0930909682770364],
-    };
-  },
-  methods: {
-    setupLeafletMap: function () {
-      const mapDiv = L.map("mapContainer").setView(this.center, 13);
-      L.tileLayer("https://tile.osm.ch/switzerland/{z}/{x}/{y}.png", {
-        maxZoom: 18,
-        attribution:
-          '&copy; <a href="https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml?lang=de">OpenStreetMap</a> contributors',
-        bounds: [
-          [45, 5],
-          [48, 11],
-        ],
-      }).addTo(mapDiv);
-    },
-  },
-  mounted() {
-    this.setupLeafletMap();
-  },
-};
+  name: "app",
+  components: {
+    myMap: Map
+  }
+}
 </script>
 
-<style scoped>
-#mapContainer {
-  width: 80vw;
-  height: 100vh;
-}
-</style>
---
+++ Frontend/vue-leaflet/src/components/Map.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="container">
+        <div id="mapContainer"></div>
+    </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default {
+    name: "Map",
+    data() {
+        return {
+            center: [46.30942535215705, 7.0930909682770364],
+        };
+    },
+    methods: {
+        setupLeafletMap: function () {
+            const mapDiv = L.map("mapContainer").setView(this.center, 13);
+            L.tileLayer("https://tile.osm.ch/switzerland/{z}/{x}/{y}.png", {
+                maxZoom: 18,
+                attribution:
+                    '&copy; <a href="https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml?lang=de">OpenStreetMap</a> contributors',
+                bounds: [
+                    [45, 5],
+                    [48, 11],
+                ],
+            }).addTo(mapDiv);
+        },
+    },
+    mounted() {
+        this.setupLeafletMap();
+    },
+};
+</script>
+
+<style scoped>
+#mapContainer {
+    width: 80vw;
+    height: 100vh;
+}
+</style>
