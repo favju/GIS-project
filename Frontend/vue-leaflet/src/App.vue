614b2542d6b5d69cce161a25abace1a5ea46e4d5
favju <julien.favre1@students.hevs.ch>
2023-05-26 14:48:39 +0200

    Leafletmap added

--- Frontend/vue-leaflet/src/App.vue
+++ Frontend/vue-leaflet/src/App.vue
@@ -1,47 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="container">
+    <div id="mapContainer"></div>
+  </div>
 </template>
 
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default {
+  name: "Map",
+  data() {
+    return {
+      center: [46.30942535215705, 7.0930909682770364],
+    };
+  },
+  methods: {
+    setupLeafletMap: function () {
+      const mapDiv = L.map("mapContainer").setView(this.center, 13);
+      L.tileLayer("https://tile.osm.ch/switzerland/{z}/{x}/{y}.png", {
+        maxZoom: 18,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        bounds: [
+          [45, 5],
+          [48, 11],
+        ],
+      }).addTo(mapDiv);
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#mapContainer {
+  width: 80vw;
+  height: 100vh;
 }
 </style>
