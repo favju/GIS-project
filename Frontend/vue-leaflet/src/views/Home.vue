64d57aef83ae89ebd5d38b0b442c192fade75b86
Gianou <david.gianadda@students.hevs.ch>
2023-06-13 17:13:51 +0200

    Slope service

--- Frontend/vue-leaflet/src/views/Home.vue
+++ Frontend/vue-leaflet/src/views/Home.vue
@@ -12,6 +12,7 @@
 <script>
 import Map from '../components/Map.vue'
 import skiliftService from '../services/skiliftService.js'
+import slopeService from '../services/slopeService.js'
 
 export default {
     name: "Home",
@@ -25,31 +26,50 @@ export default {
     },
     async mounted() {
         //L.geoJSON(this.skilift.features).addTo(this.$refs.mapy.mapDiv);
-        await skiliftService.getSkilifts();
-        console.log(skiliftService.skilifts.value)
-        console.log(this.skilifts)
+        slopeService.getSlopes();
+        skiliftService.getSkilifts();
+
     },
 
     computed: {
         skilifts() {
             return skiliftService.skilifts.value
+        },
+        slopes() {
+            return slopeService.slopes.value
         }
     },
 
     methods: {
         testAddLayer() {
-            console.log("coucou")
-            //L.geoJSON(this.skilift.features).addTo(this.$refs.mapy.mapDiv);
-
-
-            var myStyle = {
+            var myStyleBlue = {
                 "color": "blue",
+                "weight": 5,
+                "opacity": 0.65
+            };
+            var myStyleRed = {
+                "color": "red",
                 "weight": 5,
                 "opacity": 0.65
             };
 
             L.geoJSON(this.skilifts,
                 {
+                    style: myStyleBlue,
+                    onEachFeature: (feature, layer) => {
+                        layer.bindPopup(feature.properties.name);
+                        layer.bindTooltip(feature.properties.name).openTooltip();
+                        layer.on('click', (ev) => {
+                            this.changeDetail(feature.properties) // ev is an event object (MouseEvent in this case)
+
+                        });
+                    },
+
+                }).addTo(this.$refs.mapy.mapDiv);
+
+            L.geoJSON(this.slopes,
+                {
+                    style: myStyleRed,
                     onEachFeature: (feature, layer) => {
                         layer.bindPopup(feature.properties.name);
                         layer.bindTooltip(feature.properties.name).openTooltip();
@@ -60,6 +80,7 @@ export default {
 
                     }
                 }).addTo(this.$refs.mapy.mapDiv);
+
         },
 
         changeDetail(content) {
