82473f89d00ee6c543e92de44ddd507d41dba833
Gianou <david.gianadda@students.hevs.ch>
2023-06-13 16:20:41 +0200

    Model, serializer, view for slopes

--- Backend/swissgeo/models.py
+++ Backend/swissgeo/models.py
@@ -16,3 +16,16 @@ class Skilift(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Slope(models.Model):
+    id=models.PositiveIntegerField(primary_key=True)
+    name=models.CharField(max_length=200)
+    color=models.CharField(max_length=200)
+    length=models.DecimalField(max_digits=10, decimal_places=4)
+    geom=models.MultiLineStringField(srid=4326, null=True)
+
+    class Meta:
+        db_table = "slopes"
+
+    def __str__(self):
+        return self.name
--- Backend/swissgeo/serializers.py
+++ Backend/swissgeo/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Skilift
+from .models import Skilift, Slope
 from django.core.serializers import serialize
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 class UserSerializer(serializers.ModelSerializer):
@@ -11,5 +11,11 @@ class UserSerializer(serializers.ModelSerializer):
 class SkiliftSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = Skilift
+        fields = '__all__'
+        geo_field = 'geom'
+
+class SlopeSerializer(GeoFeatureModelSerializer):
+    class Meta:
+        model = Slope
         fields = '__all__'
         geo_field = 'geom'
